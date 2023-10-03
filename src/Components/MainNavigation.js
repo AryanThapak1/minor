7f901d4b8bee254cc3695867832c2db5b5ded596
Aryan Thapak <aryanthapak8@gmail.com>
2023-10-03 12:12:51 +0530

    v1.1

--- src/Components/MainNavigation.js
+++ src/Components/MainNavigation.js
@@ -1,10 +1,17 @@
+import { useNavigate} from 'react-router-dom';
 import logo from '../Images/logo.jpg'
 import classes from "./MainNavigation.module.css"
 const MainNavigation=()=>
+
 {
+    const navigate=useNavigate();
+    const logoHandler=()=>
+    {
+        navigate('/')
+    }
     return(
     <div className={classes.nav}>
-    <img src={logo} alt="logo" className={classes.img}/>
+   <img src={logo} alt="logo" className={classes.img} onClick={logoHandler}/>
     <div className={classes.code}>
     <input type="search" className={classes.input} placeholder="Enter Class Code"/>
     <button type="submit">Search</button>
